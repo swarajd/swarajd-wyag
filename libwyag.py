b8ab0f1e4bed6e8e3af6de9de4a8a311463e09db
swarajd <swaraj.dhumne@gmail.com>
2019-03-23 13:37:06 -0400

    added object read

--- libwyag.py
+++ libwyag.py
@@ -102,6 +102,35 @@ def repo_find(path=".", required=True):
     # recursive case
     return repo_find(parent, required)
 
+def object_read(repo, sha):
+    """Read object sha from Git repository repo. Return a GitObject whose exact
+    type depends on the object"""
+
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+    with open(path, "rb") as f:
+        raw = zlip.decompress(f.read())
+
+
+        # Read object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        # Read and validate object size
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw) - y - 1:
+            raise Exception("Malformed object {0}: bad length".format(sha))
+
+        # Pick constructor
+        if fmt == b'commit' : c = GitCommit
+        elif fmt == b'tree' : c = GitTree
+        elif fmt == b'tag'  : c = GitTag
+        elif fmt == b'blob' : c = GitBlob
+        else:
+            raise Exception("Unknown type %s for object %s".format(fmt.decode("ascii"), sha))
+
+
 class GitObject(object):
     """a git object"""
 
